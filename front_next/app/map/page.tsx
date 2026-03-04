cae6699e7e734b81c4ce401710dbfd560d8d907f
Kevin Jobs <me@kevinjobs.com>
2026-03-04 13:05:12 +0800

    ✨ 使用点标记图片位置
    避免渲染图片造成资源消耗大

--- front_next/app/map/page.tsx
+++ front_next/app/map/page.tsx
@@ -11,107 +11,81 @@ export default function Map() {
   const setting = useAtomValue(settingAtom);
 
   useEffect(() => {
-    // @ts-ignore
-    window._AMapSecurityConfig = {
-      securityJsCode: setting?.map?.code || "",
-    };
+    console.log(setting?.map);
+    if (setting?.map?.code && setting?.map?.key) {
+      // @ts-ignore
+      window._AMapSecurityConfig = {
+        securityJsCode: setting?.map?.code || "",
+      };
 
-    // @ts-ignore
-    AMapLoader && AMapLoader.load({
-      key: setting?.map?.key, //申请好的Web端开发者key，调用 load 时必填
-      version: "2.0", //指定要加载的 JS API 的版本，缺省时默认为 1.4.15
-    })
-      .then((AMap: any) => {
-        //JS API 加载完成后获取AMap对象
-        const map = new AMap.Map("map-container", {
-          viewMode: '2D', //默认使用 2D 模式
-          zoom: 5, //地图级别
-          center: [116.397428, 30.90923], //地图中心点
-        });
+      // @ts-ignore
+      AMapLoader &&
+        // @ts-ignore
+        AMapLoader.load({
+          key: setting?.map?.key, //申请好的Web端开发者key，调用 load 时必填
+          version: "2.0", //指定要加载的 JS API 的版本，缺省时默认为 1.4.15
+        })
+          .then((AMap: any) => {
+            //JS API 加载完成后获取AMap对象
+            const map = new AMap.Map("map-container", {
+              viewMode: "2D", //默认使用 2D 模式
+              zoom: 5, //地图级别
+              center: [118, 31], //地图中心点
+              mapStyle: "amap://styles/grey",
+            });
 
-        const addLayer = (lists: any[]) => {
-          const canvas = document.createElement("canvas");
-          const imgLayer = new AMap.CustomLayer(canvas, {
-            zooms: [3, 99],
-            zIndex: 120
-          });
-
-          const onRender = () => {
-            var size = map.getSize();//resize
-
-            var retina = AMap.Browser.retina;
-
-            var width = size.width;
-            var height = size.height;
-
-            if (retina) {
-              width *= 2;
-              height *= 2;
-            }
-
-            canvas.style.width = width + 'px'
-            canvas.style.height = height + 'px'
-
-            canvas.width = width;
-            canvas.height = height;
-
-            for (const item of lists) {
-              const ctx = canvas.getContext("2d");
-
-              if (ctx) {
-                const img = new Image();
-                img.src = genSrc(item.src);
-                img.style = "box-shadow: 0 0 4px rgba(0,0,0,0.5); border-radius: 4px;";
-                canvas.onclick = () => router.push(`/gallery/${item.uid}`);
-
-                img.onload = function () {
-                  const exif = JSON.parse(item.exif);
-                  const lat = exif.latitude?.split(",")?.[0];
-                  const lng = exif.longitude?.split(",")?.[0];
-
-                  const lngLat = new AMap.LngLat(parseFloat(lng), parseFloat(lat));
-
-                  AMap.convertFrom(lngLat, "gps", (status: string, result: any) => {
-                    if (status === "complete" && result.info === "ok") {
-                      const containerPos = map.lngLatToContainer(lngLat);
-
-                      const ratio = img.width / img.height;
-                      const imgWidth = 120;
-                      const imgHeight = imgWidth / ratio;
-
-                      ctx.drawImage(img, containerPos.x, containerPos.y, imgWidth, imgHeight);
-                      ctx.moveTo(containerPos.x, containerPos.y);
-                    }
+            const addLayer = (lists: any[]) => {
+              for (const item of lists) {
+                const exif = JSON.parse(item.exif);
+                const longitude = exif?.longitude?.split(",")[0];
+                const latitude = exif?.latitude?.split(",")[0];
+                if (latitude && longitude) {
+                  //设置圆形位置
+                  var center = new AMap.LngLat(longitude, latitude);
+                  //设置圆的半径大小
+                  var radius = 10; //单位:px
+                  //创建圆形点标记 CircleMarker 实例
+                  const circleMarker = new AMap.CircleMarker({
+                    center: center, //圆心
+                    radius: radius, //半径
+                    strokeColor: "white", //轮廓线颜色
+                    strokeWeight: 2, //轮廓线宽度
+                    strokeOpacity: 0.5, //轮廓线透明度
+                    fillColor: "rgba(0,0,255,1)", //圆点填充颜色
+                    fillOpacity: 0.5, //圆点填充透明度
+                    zIndex: 10, //圆点覆盖物的叠加顺序
+                    cursor: "pointer", //鼠标悬停时的鼠标样式
                   });
-                };
 
+                  circleMarker.on("click", () => {
+                    router.push(`/gallery/${item.uid}`);
+                  });
+
+                  //圆形 circleMarker 对象添加到 Map
+                  map.add(circleMarker);
+                }
               }
-            }
-          }
+            };
 
-          imgLayer.render = onRender;
-          imgLayer.setMap(map);
-        }
+            const fetchData = (callback: any) => {
+              getPhotoLists().then((res) => {
+                const lists = res?.lists;
 
-        const fetchData = (callback: any) => {
-          getPhotoLists().then((res) => {
-            const lists = res?.lists;
+                if (lists?.length > 0) {
+                  callback(lists);
+                }
+              });
+            };
 
-            if (lists?.length > 0) {
-              callback(lists);
-            }
+            fetchData(addLayer);
           })
-        }
-
-        fetchData(addLayer);
-      })
-      .catch((e: any) => {
-        console.error(e); //加载错误提示
-      });
-
+          .catch((e: any) => {
+            console.error(e); //加载错误提示
+          });
+    }
   }, [setting]);
-  
+
   return (
     <div id="map-container" className="w-screen h-[calc(100vh-64px)]"></div>
-  )
+  );
 }
