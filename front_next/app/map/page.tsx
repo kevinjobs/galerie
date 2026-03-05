33b3f583ca444b2f09b7a1ec12ad1a84350f6c90
Kevin Jobs <me@kevinjobs.com>
2026-03-05 12:29:25 +0800

    🐛 修复地图下边距显示问题

--- front_next/app/map/page.tsx
+++ front_next/app/map/page.tsx
@@ -1,17 +1,18 @@
 "use client";
 
-import { useEffect } from "react";
-import { genSrc, getPhotoLists } from "../api";
-import { useRouter } from "next/navigation";
-import { settingAtom } from "../store";
 import { useAtomValue } from "jotai";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { BrowserView, MobileView } from "react-device-detect";
+import { toast } from "@heroui/react";
+import { getPhotoLists } from "../api";
+import { settingAtom } from "../store";
 
 export default function Map() {
   const router = useRouter();
   const setting = useAtomValue(settingAtom);
 
   useEffect(() => {
-    console.log(setting?.map);
     if (setting?.map?.code && setting?.map?.key) {
       // @ts-ignore
       window._AMapSecurityConfig = {
@@ -41,34 +42,43 @@ export default function Map() {
                 const longitude = exif?.longitude?.split(",")[0];
                 const latitude = exif?.latitude?.split(",")[0];
 
-                if (latitude && longitude && longitude !== 'undefined' && latitude !== 'undefined') {
-                  AMap.convertFrom([longitude, latitude], "gps", (status: any, result: any) => {
-                    if (status === "complete" && result.info === "ok") {
-                      //设置圆形位置
-                      var center = result.locations[0]; //圆心坐标位置对象，格式为 {lng:xxx, lat:xxx}
-                      //设置圆的半径大小
-                      var radius = 10; //单位:px
-                      //创建圆形点标记 CircleMarker 实例
-                      const circleMarker = new AMap.CircleMarker({
-                        center: center, //圆心
-                        radius: radius, //半径
-                        strokeColor: "white", //轮廓线颜色
-                        strokeWeight: 2, //轮廓线宽度
-                        strokeOpacity: 0.5, //轮廓线透明度
-                        fillColor: "rgba(0,0,255,1)", //圆点填充颜色
-                        fillOpacity: 0.5, //圆点填充透明度
-                        zIndex: 10, //圆点覆盖物的叠加顺序
-                        cursor: "pointer", //鼠标悬停时的鼠标样式
-                      });
+                if (
+                  latitude &&
+                  longitude &&
+                  longitude !== "undefined" &&
+                  latitude !== "undefined"
+                ) {
+                  AMap.convertFrom(
+                    [longitude, latitude],
+                    "gps",
+                    (status: any, result: any) => {
+                      if (status === "complete" && result.info === "ok") {
+                        //设置圆形位置
+                        var center = result.locations[0]; //圆心坐标位置对象，格式为 {lng:xxx, lat:xxx}
+                        //设置圆的半径大小
+                        var radius = 10; //单位:px
+                        //创建圆形点标记 CircleMarker 实例
+                        const circleMarker = new AMap.CircleMarker({
+                          center: center, //圆心
+                          radius: radius, //半径
+                          strokeColor: "white", //轮廓线颜色
+                          strokeWeight: 2, //轮廓线宽度
+                          strokeOpacity: 0.5, //轮廓线透明度
+                          fillColor: "rgba(0,0,255,1)", //圆点填充颜色
+                          fillOpacity: 0.5, //圆点填充透明度
+                          zIndex: 10, //圆点覆盖物的叠加顺序
+                          cursor: "pointer", //鼠标悬停时的鼠标样式
+                        });
 
-                      circleMarker.on("click", () => {
-                        router.push(`/gallery/${item.uid}`);
-                      });
+                        circleMarker.on("click", () => {
+                          router.push(`/gallery/${item.uid}`);
+                        });
 
-                      //圆形 circleMarker 对象添加到 Map
-                      map.add(circleMarker);
-                    }
-                  });
+                        //圆形 circleMarker 对象添加到 Map
+                        map.add(circleMarker);
+                      }
+                    },
+                  );
                 }
               }
             };
@@ -86,12 +96,19 @@ export default function Map() {
             fetchData(addLayer);
           })
           .catch((e: any) => {
-            console.error(e); //加载错误提示
+            toast.danger(String(e)); //加载错误提示
           });
     }
   }, [setting]);
 
   return (
-    <div id="map-container" className="w-screen h-[calc(100vh-64px)]"></div>
+    <>
+      <BrowserView>
+        <div id="map-container" className="w-screen h-[calc(100vh-64px)]"></div>
+      </BrowserView>
+      <MobileView>
+        <div id="map-container" className="w-screen h-[calc(100vh-56px)]"></div>
+      </MobileView>
+    </>
   );
 }
